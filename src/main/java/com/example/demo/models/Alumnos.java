84e5db06c1a8fef3fbbe4ddf5c08bd46957e1e16
AlexCaamal <nardinchuc271000@gmail.com>
2023-05-09 14:40:36 -0600

    Segundo commit

---
+++ src/main/java/com/example/demo/models/Alumnos.java
@@ -0,0 +1,33 @@
+package com.example.demo.models;
+
+public class Alumnos {
+	private Long id;
+	private String nombre;
+	private String mail;
+	private Integer prioridad;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public Integer getPrioridad() {
+		return prioridad;
+	}
+	public void setPrioridad(Integer prioridad) {
+		this.prioridad = prioridad;
+	}
+}
